225693a5d4b567e2b397d797d97458ab3e19ddc5
Thomas Huysmans <thomas.huysmans@gmail.com>
2017-10-10 20:44:17 +0200

    Init

---
+++ ParalellTestFramework/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParalellTestFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Application");
+            var numbers = new List<int>();
+
+            for (var i = 0; i < 1000000; i++)
+            {
+                var randomGenerator = new Random(10000000);
+                numbers.Add(randomGenerator.Next());
+            }
+
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+
+
+            //Parallel.ForEach(numbers, (number) =>
+            //{
+            //    Console.WriteLine($"Number: {number}");
+            //});
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"Number: {number}");
+            }
+
+            stopWatch.Stop();
+
+
+            Console.WriteLine("Run = " + stopWatch.ElapsedMilliseconds);
+
+            Console.WriteLine("End Application");
+            Console.ReadLine();
+
+        }
+    }
+}
